f2f52e52c20872d48d0525c338d541ebefb2665c
Daniel Galvis <34688953+danielgalvis98@users.noreply.github.com>
2020-07-28 16:42:08 -0500

    Set implicit timeout wait (#12)

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -19,29 +19,19 @@ describe('Buy a t-shirt', () => {
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
     await menuContentPage.goToTshirtMenu();
-    await(browser.sleep(3000));
     await productListPage.addShirtToCart();
-    await(browser.sleep(3000));
     await productAddedModalPage.proccedToCheckout();
-    await(browser.sleep(3000));
     await summaryStepPage.confirmAndCheckout();
-    await(browser.sleep(3000));
 
     await signInStepPage.signIn('aperdomobo@gmail.com', 'WorkshopProtractor');
-    await(browser.sleep(3000));
 
     await addresStepPage.goToShippingStep();
-    await(browser.sleep(3000));
 
     await shippingStepPage.acceptTermsOfService();
-    await(browser.sleep(3000));
 
     await shippingStepPage.goToPaymentMenu();
-    await(browser.sleep(3000));
     await paymentStepPage.payByBankwire();
-    await(browser.sleep(3000));
     await bankPaymentPage.confirmOrder();
-    await(browser.sleep(3000));
 
     await expect(orderSummaryPage.getTitle())
       .toBe('Your order on My Store is complete.');
