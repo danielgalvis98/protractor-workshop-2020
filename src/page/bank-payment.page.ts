40f9619a0e63f9c6b0533d69c7400c793c422207
Daniel Galvis <dangaltor@gmail.com>
2020-07-29 17:32:19 -0500

    Bank payment and order summary pages improved locators

--- src/page/bank-payment.page.ts
+++ src/page/bank-payment.page.ts
@@ -4,6 +4,9 @@ export class BankPaymentPage {
   private confirmOrderButton: ElementFinder;
 
   constructor () {
+    /* Here the button doesn't have any id, title or name, so it is difficult to locate it
+    on a better way than the one already implemented. Anyway, I would recommend to the developper to
+    include some id to the button */
     this.confirmOrderButton = $('#cart_navigation > button > span');
   }
 
