ceb3909cc5ccac9d314de2422c9240a85139e138
Daniel Galvis <dangaltor@gmail.com>
2020-07-30 13:57:03 -0500

    Jasmine Awesome Report implemented

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,6 +1,13 @@
 import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
+import { AwesomeReport } from 'jasmine-awesome-report';
 
 export let reporter = () => {
+  const config = {
+    fullPath: 'awesome-report',
+    filename: 'report',
+    merge: true,
+  };
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: StacktraceOption.PRETTY
